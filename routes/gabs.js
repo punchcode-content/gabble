102dfa053a5e8d7516cbcddd763b1be81646187b
Clinton Dreisbach <clinton@dreisbach.us>
2017-06-25 16:18:04 -0400

    Add skeleton routes

---
+++ routes/gabs.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const models = require("../models");
+
+const router = express.Router();
+
+// show all gabs
+router.get("/", function (req, res) {
+
+});
+
+// create new gab
+router.post("/", function (req, res) {
+
+});
+
+// form for new gab
+router.get("/new", function (req, res) {
+
+});
+
+// show an individual gab and its likes
+router.get("/:gabId", function (req, res) {
+
+});
---
+++ routes/users.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const models = require("../models");
+
+const router = express.Router();
+
+// registration form
+router.get("/signup", function (req, res) {
+
+});
+
+// create new user
+router.post("/signup", function (req, res) {
+
+});
+
+// sign in form
+router.get("/signin", function (req, res) {
+
+});
+
+// sign in
+router.post("/signin", function (req, res) {
+
+});
