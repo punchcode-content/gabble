f3a1c700789c711a2600ba1ad60f64ef09b0aaaa
Clinton Dreisbach <clinton@dreisbach.us>
2017-06-26 12:19:55 -0400

    Add ability to create new gabs

--- routes/gabs.js
+++ routes/gabs.js
@@ -3,20 +3,44 @@ const models = require("../models");
 
 const router = express.Router();
 
+function checkAuth(req, res, next) {
+  if (req.user) {
+    next()
+  } else {
+    res.redirect("/users/login")
+  }
+}
+
 // show all gabs
 router.get("/", function (req, res) {
-  console.log("req.user", req.user)
-  res.render("gabs/index");
+  models.Gab.findAll({
+    order: [['createdAt', 'DESC']],
+    include: [{model: models.User}]
+  }).then(function (gabs) {
+    res.render("gabs/index", {gabs: gabs});
+  });
 });
 
 // create new gab
-router.post("/", function (req, res) {
+router.post("/", checkAuth, function (req, res) {
+  req.checkBody("body", "Gab must not be empty.").notEmpty();
+  req.checkBody("body", "Gab must be 140 characters or less.").isLength({max: 140});
 
+  req.getValidationResult().then(function (result) {
+    if (result.isEmpty()) {
+      const gab = models.Gab.build({body: req.body.body, userId: req.user.id})
+      gab.save().then(function (newGab) {
+        res.redirect("/gabs")
+      })
+    } else {
+      res.render("gabs/new", {errors: result.mapped(), body: req.body.body});
+    }
+  });
 });
 
 // form for new gab
-router.get("/new", function (req, res) {
-
+router.get("/new", checkAuth, function (req, res) {
+  res.render("gabs/new");
 });
 
 // show an individual gab and its likes
